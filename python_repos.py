facc62b1c39a915f8d1def2c51c6f6c61732e20e
Mwa-ura <101228726+Mwa-ura@users.noreply.github.com>
2023-02-27 10:04:40 +0400

    Explore python repositories dict

--- python_repos.py
+++ python_repos.py
@@ -12,3 +12,13 @@ response_dict = r.json()
 # process results
 print(response_dict.keys())
 print(response_dict['total_count'])
+
+#Explore information about repositories
+repo_dicts = response_dict['items']
+print(F"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository
+repo_1 = repo_dicts[0]
+print(F"\nKeys: {len(repo_1)}")
+for key in sorted(repo_1.keys()):
+	print(key)
